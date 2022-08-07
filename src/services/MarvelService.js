1d5674d3e747c0e3588dc111dff6733fff40eb10
Aliaksey-Iliukevich <ilykevich.aleksey@gmail.com>
2022-08-07 14:20:14 +0300

    feat: random char

---
+++ src/services/MarvelService.js
@@ -0,0 +1,37 @@
+class MarvelService {
+    _apiBase = 'https://gateway.marvel.com:443/v1/public/';
+    
+    _apiKey = 'apikey=74e4eb9119efd3aa3a1d9ced15685165';
+
+    getResource = async (url) => {
+        let res = await fetch(url);
+    
+        if (!res.ok) {
+            throw new Error(`Could not fetch ${url}, status: ${res.status}`);
+        }
+    
+        return await res.json();
+    }
+
+    getAllCharacters = async () => {
+        const res = await this.getResource(`${this._apiBase}characters?limit=9&offset=195&${this._apiKey}`);
+        return res.data.results.map(this._transformCharacter);
+    }
+
+    getCharacter = async (id) => {
+        const res = await this.getResource(`${this._apiBase}characters/${id}?${this._apiKey}`);
+        return this._transformCharacter(res.data.results[0]);
+    }
+
+    _transformCharacter = (char) => {
+        return {
+            name: char.name,
+            description: char.description ? `${char.description.slice(0, 180)}...` : 'Unfortunately, we don’t know anything about this character...',
+            thumbnail: char.thumbnail.path + '.' + char.thumbnail.extension,
+            homepage: char.urls[0].url,
+            wiki: char.urls[1].url
+        }
+    }
+}
+
+export default MarvelService;
